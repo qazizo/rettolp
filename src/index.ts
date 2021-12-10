16d1f654350c8904c89bfe121bf8f13dbdb30788
zizo <>
2021-12-10 18:54:00 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {render} from 'react-dom';
+import {createElement} from 'react';
+import './index.css';
+import App from './components/app';
+
+render(createElement(App), document.getElementById('root'));
